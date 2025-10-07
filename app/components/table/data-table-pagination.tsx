22d47a8628abad7fff1e55245d047d1d201b8d6d
Tiago <tiagotbw@gmail.com>
2025-10-07 14:33:16 -0300

    refactor: update Devices Data layout to use NavMenu and improve structure

--- app/components/table/data-table-pagination.tsx
+++ app/components/table/data-table-pagination.tsx
@@ -24,7 +24,7 @@ export function DataTablePagination<TData>({
 }: DataTablePaginationProps<TData>) {
   return (
     <div className='flex items-center justify-between px-2'>
-      <div className='flex items-center space-x-6 lg:space-x-8'>
+      <div className='flex items-center justify-end flex-wrap gap-6'>
         <div className='flex items-center space-x-2'>
           <p className='text-sm font-medium'>Linhas por página</p>
           <Select
--- app/routes/devices-data/layout.tsx
+++ app/routes/devices-data/layout.tsx
@@ -1,26 +1,35 @@
-import { Outlet, useParams } from 'react-router';
+import { ArrowLeft } from 'lucide-react';
+import { Link, Outlet, useParams } from 'react-router';
 
 import NavMenuTitle from '~/components/layout/nav-menu-title';
+import NavMenu, { NAV_LINK_PADDING } from '~/components/nav-menu';
 
 export default function DevicesDataLayout() {
   const params = useParams();
   const { id } = params;
+  const navigationLinks = [
+    {
+      to: `/devices-data/${id}/read-data`,
+      content: 'Ler Dados',
+    },
+    {
+      to: `/devices-data/${id}/program`,
+      content: 'Programar',
+    },
+  ];
 
   return (
     <>
-      <NavMenuTitle
-        title={''}
-        navigationLinks={[
-          {
-            to: `/devices-data/${id}/read-data`,
-            content: 'Ler Dados',
-          },
-          {
-            to: `/devices-data/${id}/program`,
-            content: 'Programar',
-          },
-        ]}
-      />
+      <div className='flex justify-between items-center bg-gray-100 pb-2 px-4 lg:px-16'>
+        <Link to='/' prefetch='intent'>
+          <div
+            className={`flex items-center gap-2 font-semibold ${NAV_LINK_PADDING}`}
+          >
+            <ArrowLeft /> Voltar
+          </div>
+        </Link>
+        <NavMenu navigationLinks={navigationLinks} />
+      </div>
       <Outlet />
     </>
   );
